88a55d64dec93f4eb07b20208659807278742480
AminulAnik <imaminul012@gmail.com>
2023-12-20 10:54:04 +0600

    header part added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          clifford: '#da373d',
-        }
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        headerbg: "#282828",
+      },
+      maxWidth: {
+        container: "1140px",
+      },
+      fontFamily: {
+        'poppins': "'Poppins', sans-serif",
       }
-    }
-  }
+    },
+  },
+};
